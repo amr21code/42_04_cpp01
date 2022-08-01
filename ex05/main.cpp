d21ac7f70fe1df69cc166c7eb5f1ecb6c5ea90b4
anruland <anruland@student.42wolfsburg.de>
2022-08-01 15:35:43 +0200

    ex05 added tests

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anruland <anruland@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 14:43:08 by anruland          #+#    #+#             */
-/*   Updated: 2022/08/01 15:20:40 by anruland         ###   ########.fr       */
+/*   Updated: 2022/08/01 15:32:22 by anruland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,27 @@
 
 int	main(int ac, char **av)
 {
-	Harl	Harl;
+	Harl		Harl;
+	std::string command;
 
 	if (ac == 2)
-		Harl.Complain(av[1]);
+	{
+		command = av[1];
+		if (command.compare("tests"))
+			Harl.Complain(av[1]);
+		else
+		{
+			Harl.Complain("DEBUG");
+			Harl.Complain(" ");
+			Harl.Complain("BS");
+			Harl.Complain("debug");
+			Harl.Complain("INFO");
+			Harl.Complain("ERROR");
+			Harl.Complain("WARNING");
+			Harl.Complain("INFO");
+		}
+	}
 	else
-		std::cout << "usage: ./harl <complaint>" << std::endl;
+		std::cout << "usage: ./harl <complaint> (or <tests> for automated tests, without <>" << std::endl;
 	return (0);
 }
